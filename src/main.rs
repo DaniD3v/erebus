149faf8238839d4265d243534662724509f919f8
DaniD3v <sch220233@spengergasse.at>
2024-09-09 13:22:28 +0200

    added error handling with ariadne

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::fs;
+use std::{fs, path::Path};
 
+use ariadne::{sources, Color, Label, Report, ReportKind};
 use clap::Parser as ClapParser;
 
 use args::{Args, Emit};
@@ -8,12 +9,45 @@ use parser::{Ast, Parsable};
 mod args;
 mod parser;
 
+fn failed_compiling(input_file: &Path, error_amount: usize) -> ! {
+    assert!(error_amount > 0);
+
+    panic!(
+        "failed compiling \"{}\" due to {}.",
+        input_file.display(),
+        match error_amount {
+            1 => "1 error".to_owned(),
+            x => format!("{x} errors."),
+        },
+    )
+}
+
 fn main() {
     let args = Args::parse();
     let input_content = fs::read_to_string(&args.input_file)
         .unwrap_or_else(|_| panic!("failed to read {:#?}", &args.input_file));
 
-    let ast = Ast::parse(&input_content).unwrap();
+    let ast = match Ast::parse(&input_content).into_result() {
+        Ok(ast) => ast,
+        Err(errors) => {
+            let filename = args.input_file.display().to_string();
+
+            for err in &errors {
+                Report::build(ReportKind::Error, filename.clone(), err.span().start)
+                    .with_message(err.to_string())
+                    .with_label(
+                        Label::new((filename.clone(), err.span().into_range()))
+                            .with_message(err.reason().to_string())
+                            .with_color(Color::Red),
+                    )
+                    .finish()
+                    .print(sources([(filename.clone(), input_content.clone())]))
+                    .unwrap()
+            }
+
+            failed_compiling(&args.input_file, errors.len())
+        }
+    };
 
     if matches!(args.emit, Emit::Ast) {
         println!("Ast: {:#?}", ast)
